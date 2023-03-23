3ee39ba550e817c5c96c7af2eb5c80730c2f33e5
Sai Prasad Sappati <bunny.sd62@gmail.com>
2023-03-23 17:11:35 +0530

    changes

--- app/serializers/role_serializer.rb
+++ app/serializers/role_serializer.rb
@@ -1,7 +1,3 @@
 class RoleSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
   attributes :role
-=======
-  attributes :id,:role
->>>>>>> dev
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,13 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
   	attributes :id, :emp_id, :email
 		has_one :user_detail
 		has_one :role
 		has_many :devices
-=======
-  attributes :id,:emp_id,:email
-  has_one :user_detail
-  has_one :role
-  has_many :devices
->>>>>>> dev
 end
