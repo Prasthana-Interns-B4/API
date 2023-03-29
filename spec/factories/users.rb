f1bb06dac0eb91795e39f216affc9c32c592950d
Sai Prasad Sappati <bunny.sd62@gmail.com>
2023-03-29 22:29:05 +0530

    controller specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     emp_id { "PR001" }
     email { Faker::Internet.email }
     password { 'Prasthana@2023' }
-    status { "pending" }
+    status { "active" }
 
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,8 +4,7 @@ require 'json'
 RSpec.describe "UsersController", type: :request do
 	let(:user) { create :user }
 	let(:user1) { create :user }
-	let(:user2) { create :user }
-	let(:role) { create :user.role }
+	# let(:role) { create :user.role }
 	let(:url) {'/users/sign_in'}
 	let(:params) do
 		{
@@ -37,13 +36,21 @@ RSpec.describe "UsersController", type: :request do
 	  end
 	end
 
-	# describe "GET /users/pending" do
-	# 	  it "should display all pending users" do
-	# 		# user = create(:user)
-	#     get pending_users_path
-	#     expect(response).to have_http_status(200)
-	#   end
-	# end
+	describe "GET /users/pending" do
+		before do
+			post url, params: params.to_json, headers: {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+		end
+		it "should display pending users if user is HR Manager only" do
+			user.role = Role.create(role: 'hr_manager')
+	    get pending_users_path, headers: { 'Authorization' => response.headers['Authorization']}
+	    expect(response).to have_http_status(200)
+	  end
+		it "should not display pending users if user is not HR Manager" do
+			user.role = Role.create(role: 'facility_manager')
+			get pending_users_path, headers: { 'Authorization' => response.headers['Authorization']}
+	    expect(response).to have_http_status(401)
+		end
+	end
 
   describe "PUT /users/forgot_password" do
     it "should check the emp_id and reset password" do
